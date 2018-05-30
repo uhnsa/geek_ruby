cff9fb7bc28802618928c1424cbf939b2693fd89
uhnsa <39530129+uhnsa@users.noreply.github.com>
2018-05-30 17:02:40 +0300

    Update task1.js
    удалено несколько строк, но программа всё равно работает

--- task1.js
+++ task1.js
@@ -1,9 +1,4 @@
 var str =process.argv[2];
-//str =process.argv[2];
-/*str.replace(/р/g,"");
-str.replace(/к/g,"");
-str.replace(/н/g,"");*/
-//process.stdout.write(a);
 var nstr="";
 for(var i=0; i<str.length; i++){
 	if(str[i]=="к"||str[i]=="р"||str[i]=="н"){
