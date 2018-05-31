a0df8d13ec29e0c8aa920e5f258e7839e57bb8a6
uhnsa <39530129+uhnsa@users.noreply.github.com>
2018-05-31 17:14:06 +0300

    Update task1.js
    улучшил условие

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 var str =process.argv[2];
 var nstr="";
 for(var i=0; i<str.length; i++){
-	if(str[i]=="к"||str[i]=="р"||str[i]=="н"){
+	if(str[i]=="к"||str[i]=="р"||str[i]=="н"||str[i]=="К"||str[i]=="Р"||str[i]=="Н"){
 		}else{
 			nstr=nstr+str[i];
 		}
