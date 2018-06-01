b8a32fa05f25104d3808177dd5e014520590e9a4
uhnsa <39530129+uhnsa@users.noreply.github.com>
2018-06-01 16:13:20 +0300

    Delete task1.js

--- task1.js
+++
@@ -1,9 +0,0 @@
-var str =process.argv[2];
-var nstr="";
-for(var i=0; i<str.length; i++){
-	if(str[i]=="к"||str[i]=="р"||str[i]=="н"||str[i]=="К"||str[i]=="Р"||str[i]=="Н"){
-		}else{
-			nstr=nstr+str[i];
-		}
-}
-process.stdout.write(nstr);
